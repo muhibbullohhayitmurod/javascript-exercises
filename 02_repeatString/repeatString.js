f7f034f65164ecfb48f59499711e80f1ed708444
Muhibbulloh Hayitmurod <181156023+muhibbullohhayitmurod@users.noreply.github.com>
2024-11-29 11:52:41 +0500

    Add solution to ex. 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+  if (num >= 0) {
+    let emptyStr = "";
+    for (let i = 0; i < num; i++) {
+      emptyStr += str;
+    }
+    return emptyStr;
+  }else{
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
