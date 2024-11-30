f4c328e41142fa50c4e10324dedfcba5e790d2c5
Muhibbulloh Hayitmurod <181156023+muhibbullohhayitmurod@users.noreply.github.com>
2024-12-01 02:01:23 +0500

    Add solution to ex.3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  let reversedStr = "";
+  const strArr = str.split("");
+  for (let i = strArr.length - 1; i >= 0; i--) {
+    reversedStr += strArr[i];
+  }
+  return reversedStr;
 };
-
 // Do not edit below this line
 module.exports = reverseString;
