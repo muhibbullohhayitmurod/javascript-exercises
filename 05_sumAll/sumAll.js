05863ca8d6ef482f1ff96cb2b35432d02cb2bd53
Muhibbulloh Hayitmurod <181156023+muhibbullohhayitmurod@users.noreply.github.com>
2024-12-02 11:53:25 +0500

    Add solution to ex.05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0;
+    if (start < 0 || end < 0) return "ERROR";
+    if(!Number.isInteger(start) || !Number.isInteger(end)) return "ERROR";
+   
+    if (start > end){
+        const temp = start;
+        start = end;
+        end = temp;
+    }
 
+    while(start <= end){
+        sum += start;
+        if(isNaN(sum)){
+            return "ERROR";
+        }
+        start++;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
